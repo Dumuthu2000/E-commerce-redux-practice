9632adac33ad31bfddccaaaafcf50d590adc498c
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-12-09 11:57:40 +0530

    Set cart items count using cart state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { useSelector } from "react-redux"
 
 const Navbar = () => {
+    const { user } = useSelector((state) => state.auth);
+    const { cart } = useSelector((state) => state.cart);
   return (
     <nav>
-      <div style={{display: 'flex', justifyContent:'space-evenly'}}>
+      <div style={{
+        display: 'flex', 
+        justifyContent:'space-evenly',
+        alignItems: 'center'
+        }}>
         <h1>Test E commerce</h1>
-        <p>Cart: 0</p>
+        <p>User: {user.username}</p>
+        <p>Cart: {cart.length}</p>
       </div>
     </nav>
   )
