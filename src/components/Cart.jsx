d9a7fed9a72b36302a2655b3a618a05018ef0fa7
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-12-09 12:12:29 +0530

    Add total amount to display in the cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const Cart = () => {
-    const { cart } = useSelector((state) => state.cart);
+    const { cart, total } = useSelector((state) => state.cart);
   return (
     <div>
+        <h2>CART DETAILS</h2>
       {cart.map((item, index) => {
         return (
             <li key={index}>{item.name}</li>
         )
       })}
+      <hr />
+      <h2>TOTAL AMOUNT</h2>
+      <p>Rs: {total}</p>
     </div>
   )
 }
