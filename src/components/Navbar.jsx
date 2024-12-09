2aebdbb0a033673e40e5b5ff56f2b7391488a614
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-12-09 17:19:42 +0530

    Add logout functionality by use logout reducer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { logout } from "../store/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
     const { user } = useSelector((state) => state.auth);
     const { cart } = useSelector((state) => state.cart);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+      dispatch(logout());
+      navigate('/login')
+    }
   return (
     <nav>
       <div style={{
@@ -18,6 +28,7 @@ const Navbar = () => {
         <Link to={'/cart'}>
             <p>Cart: {cart.length}</p>
         </Link>
+        <button onClick={handleLogout}>Logout</button>
       </div>
     </nav>
   )
